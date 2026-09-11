309164d933113bfc68b730916e75007c6dc1f772
Gustavaopere <gustavaopere@gmail.com>
2026-09-11 02:45:12 -0300

    build(art): include NeoForge native animation core

--- art/tooling/blockbench/asset-toolkit/build_toolkit_bundle.js
+++ art/tooling/blockbench/asset-toolkit/build_toolkit_bundle.js
@@ -21,6 +21,7 @@ const SOURCE_MODULES = Object.freeze([
   'core/provider-profile/provider_profiles.js',
   'core/provider-adapter/geckolib4_adapter.js',
   'core/provider-adapter/azurelib3_adapter.js',
+  'core/provider-adapter/neoforge_native_animation_adapter.js',
   'core/index.js',
   'live-bridge/protocol.js',
   'live-bridge/project_snapshot.js',
