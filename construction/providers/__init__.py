0ed671bb1185705bd0135abe448cce41b4b14be6
Gustavaopere <gustavaopere@gmail.com>
2026-09-12 15:23:55 -0300

    feat(construction): add C10 handoff requests

--- construction/providers/__init__.py
+++ construction/providers/__init__.py
@@ -1,13 +1,16 @@
 from construction.providers.common import canonical_json_bytes, sha256_hex
 from construction.providers.errors import C10Error
+from construction.providers.handoff import build_handoff_request, validate_handoff_request
 from construction.providers.profiles import load_profile_catalog, profile_fingerprint, validate_profile, validate_profile_catalog
 
 __all__ = [
     "C10Error",
+    "build_handoff_request",
     "canonical_json_bytes",
     "load_profile_catalog",
     "profile_fingerprint",
     "sha256_hex",
+    "validate_handoff_request",
     "validate_profile",
     "validate_profile_catalog",
 ]
